601f974b27fa7edb791a05ab2c8d593e48694e68
WiresawBlade <wiresaw_blade@126.com>
2024-10-26 23:02:12 +0800

    feat: 使用分离的组件样式

--- src/components/utils/user-textbox.vue
+++ src/components/utils/user-textbox.vue
@@ -1,5 +1,5 @@
 <template>
-    <component :is="props.mutiLines? 'textarea' : 'input'" class="user-textbox"
+    <component :is="props.mutiLines ? 'textarea' : 'input'" class="user-textbox"
         :class="{ 'lodash-style': props.lodashStyle }" type="text" :value="modelValue"
         @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)" autocomplete="off">
     </component>
@@ -20,29 +20,10 @@ const props = withDefaults(defineProps<Props>(), {
 defineEmits(["update:modelValue"]);
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+@use "@/stylesheets/components/user-textbox" as *;
+
 .user-textbox {
-    box-sizing: border-box;
-    padding: 4px;
-    border: 2px solid var(--border-color);
-    border-radius: 6px;
-    background-color: var(--default-background);
-    outline: none;
-    transition: 0.4s ease;
-
-    &.lodash-style {
-        padding: 0;
-        border: none;
-        border-radius: 0;
-        border-bottom: 2px solid var(--border-color);
-    }
-}
-
-.user-textbox:hover {
-    border-color: var(--light-background);
-}
-
-.user-textbox:focus {
-    border-color: var(--tieba-theme-color);
+    @extend %user-textbox;
 }
 </style>
