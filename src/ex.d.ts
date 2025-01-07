92080cc1a695d22b46beb2f141ce7186e382b48d
HacksawBlade <hacksaw_blade@126.com>
2025-01-07 16:59:29 +0800

    feat: 扩展类型 `SupportedComponent`

--- src/ex.d.ts
+++ src/ex.d.ts
@@ -1,6 +1,11 @@
+import { Component } from "vue";
+import { JSX } from "vue/jsx-runtime";
 import { SubSettingKey } from "./components/settings.vue";
 
 /** 用户模块（扩展） */
 interface UserModuleEx extends UserModule {
     settings?: SubSettingKey["content"]
 }
+
+/** 支持的组件类型 */
+type SupportedComponent = Component | JSX.Element;
