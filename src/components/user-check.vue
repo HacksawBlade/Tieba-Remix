94120c3e459b6efefcbb3fc98333cc3429a441d4
HacksawBlade <hacksaw_blade@126.com>
2025-01-13 10:39:36 +0800

    feat: 复选框组件

---
+++ src/components/user-check.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="user-check">
+        <ToggleButton v-model="model" :id="id" class="check-button icon" :theme-style="model">
+            {{ model ? "check" : "" }}
+        </ToggleButton>
+        <label :for="id">{{ text }}</label>
+    </div>
+</template>
+
+<script lang="tsx" setup>
+import ToggleButton from "./utils/toggle-button.vue";
+
+interface UserCheckOpts {
+    id?: string;
+    text?: string;
+}
+
+withDefaults(defineProps<UserCheckOpts>(), {
+    text: "",
+});
+
+const model = defineModel<boolean>({
+    required: true,
+    default: false,
+});
+</script>
+
+<style lang="scss" scoped>
+@use "@/stylesheets/main/animations.scss" as *;
+
+.user-check {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+
+    .check-button {
+        width: 16px;
+        height: 16px;
+        padding: 0;
+        border-radius: 4px;
+        font-size: 14px;
+        font-weight: bold;
+    }
+}
+</style>
