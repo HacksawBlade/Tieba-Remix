b07542eee485e5d20d8638ddc9f975b519eea628
HacksawBlade <hacksaw_blade@126.com>
2025-01-19 01:33:42 +0800

    feat: 选择器组件 显示选项描述/焦点事件改进/动画修正/样式调整

--- src/components/user-select.vue
+++ src/components/user-select.vue
@@ -1,24 +1,28 @@
 <template>
     <div class="user-select">
-        <ToggleButton ref="selectToggle" v-model="useSelect" class="select-toggle">
+        <ToggleButton ref="selectToggle" v-model="useSelect" class="select-toggle" @focusout="selectFocusout">
             <p class="current-text">{{ data[currentIndex].text }}</p>
-            <div class="icon">{{ useSelect ? "keyboard_arrow_up" : "keyboard_arrow_down" }}</div>
+            <div class="icon">keyboard_arrow_down</div>
         </ToggleButton>
         <Transition name="select">
             <div ref="selectContainer" v-show="useSelect" class="select-container">
-                <UserButton v-for="(d, i) in data" :theme-style="currentIndex === i" class="select-button"
-                    @click="currentIndex = i, useSelect = false">
-                    {{ d.text }}
+                <UserButton v-for="(value, i) in data" :theme-style="currentIndex === i"
+                    class="select-transition select-button" @click="currentIndex = i, useSelect = false"
+                    @mouseenter="hoverIndex = i" @focusout="selectFocusout">
+                    {{ value.text }}
                 </UserButton>
+                <div v-show="data[hoverIndex].desc" class="select-transition select-desc" tabindex="-1"
+                    @focusout="selectFocusout">
+                    {{ useSelect ? data[hoverIndex].desc : "" }}
+                </div>
             </div>
         </Transition>
     </div>
 </template>
 
 <script lang="tsx" setup>
-import { EventProxy } from "@/lib/elemental/event-proxy";
 import { findIndex } from "lodash-es";
-import { onBeforeUnmount, onMounted, ref, watch } from "vue";
+import { ref, watch } from "vue";
 import ToggleButton from "./utils/toggle-button.vue";
 import UserButton from "./utils/user-button.vue";
 
@@ -41,8 +45,6 @@ defineModel<UserSelectItem["value"]>("value");
 
 const emit = defineEmits<{ (e: "change", value: ValueType): void }>();
 
-const evproxy = new EventProxy();
-
 const selectToggle = ref<InstanceType<typeof ToggleButton>>();
 const selectContainer = ref<HTMLDivElement>();
 const useSelect = ref(false);
@@ -51,29 +53,39 @@ const currentIndex = ref((function () {
     const index = findIndex(props.data, (d) => d.value === props.defaultValue);
     return index === -1 ? 0 : index;
 })());
+const hoverIndex = ref(currentIndex.value);
 
-onMounted(function () {
-    evproxy.on(
-        selectToggle.value?.$el as HTMLButtonElement,
-        "blur",
-        (e: FocusEvent) => {
-            if (selectContainer.value?.contains(e.relatedTarget as Node)) return;
-            useSelect.value = false;
-        }
-    );
-});
-
-onBeforeUnmount(function () {
-    evproxy.release();
+watch(useSelect, function () {
+    hoverIndex.value = currentIndex.value;
 });
 
 watch(currentIndex, index => {
     emit("change", props.data[index].value);
 });
+
+function selectFocusout(e: FocusEvent) {
+    selectContainer.value?.contains(e.relatedTarget as Node)
+        ? useSelect.value = true
+        : useSelect.value = false;
+}
 </script>
 
 <style lang="scss" scoped>
 $select-gap: 4px;
+
+@keyframes select-arrow-down {
+    0% {
+        transform: translateY(0);
+    }
+
+    70% {
+        transform: translateY(10%);
+    }
+
+    100% {
+        transform: translateY(0);
+    }
+}
 
 .user-select {
     position: relative;
@@ -90,14 +102,16 @@ $select-gap: 4px;
             background-color: var(--default-background);
             box-shadow: 0 0 0 1px var(--tieba-theme-color);
             color: var(--default-fore);
+
+            .icon {
+                animation: select-arrow-down var(--fast-duration) cubic-bezier(0.18, 0.89, 0.32, 1.6);
+                color: var(--tieba-theme-color);
+                font-weight: bold;
+            }
         }
 
         &.toggle-off {
             box-shadow: 0 0 0 1px var(--border-color);
-        }
-
-        .icon {
-            font-weight: bold;
         }
     }
 
@@ -114,32 +128,28 @@ $select-gap: 4px;
         background-color: var(--default-background);
         box-shadow: 0 0 10px rgb(0 0 0 / 20%);
 
-        &.select-enter-active .select-button,
-        &.select-leave-active .select-button {
-            transition: transform var(--fast-duration) ease-out, opacity var(--fast-duration);
+        &.select-enter-active,
+        &.select-leave-active {
+            transition: transform var(--fast-duration), opacity var(--fast-duration);
         }
 
-        &.select-enter-from .select-button,
-        &.select-leave-to .select-button {
+        &.select-enter-from,
+        &.select-leave-to {
             opacity: 0;
-            transform: translateY(-20%);
-        }
-
-        @keyframes select-out {
-            100% {
-                opacity: 0;
-                transform: translateY(-20%);
-            }
-        }
-
-        &.closing .select-button {
-            animation: select-out var(--fast-duration);
+            transform: scale(0.95) translateY(-10%);
         }
 
         .select-button {
             border-radius: 0;
             box-shadow: none;
             text-align: justify;
+        }
+
+        .select-desc {
+            padding: 4px 14px;
+            border-top: 1px solid var(--border-color);
+            color: var(--light-fore);
+            font-size: 14px;
         }
     }
 }
