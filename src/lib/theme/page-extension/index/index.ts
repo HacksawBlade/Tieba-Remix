5a7b5fd8de6f8eb8b0f02d7f399aad156b16d961
0x0b1ade <hacksaw_blade@126.com>
2026-05-30 00:17:24 +0800

    fix: 修复主页无法执行多实例检测的问题

--- src/lib/theme/page-extension/index/index.ts
+++ src/lib/theme/page-extension/index/index.ts
@@ -1,7 +1,5 @@
-import { GM_addStyle } from "$";
 import { currentPageType } from "@/lib/api/remixed";
 import { asyncdom } from "@/lib/elemental";
-import { parseMultiCSS } from "@/lib/elemental/styles";
 import { renderPage } from "@/lib/render";
 import { pageExtension } from "@/lib/user-values";
 import Home from "./index.vue";
@@ -13,17 +11,7 @@ export default async function () {
     if (!pageExtension.get().index) {
         return;
     }
-
-    const bodyMask = GM_addStyle(
-        parseMultiCSS({
-            body: {
-                display: "none",
-            },
-        }),
-    );
-
     const wrap = await asyncdom(".wrap1");
     renderPage(Home);
     wrap.remove();
-    bodyMask.remove();
 }
