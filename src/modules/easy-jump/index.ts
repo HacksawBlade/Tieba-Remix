38cc6a66aa6e66fc1946f3fa229dee8acf846115
0xb1aded <hacksaw_blade@126.com>
2026-08-10 10:20:46 +0800

    feat(module/easy-jump): 直链跳转模块支持新的安全跳转页面

--- src/modules/easy-jump/index.ts
+++ src/modules/easy-jump/index.ts
@@ -1,6 +1,9 @@
 import { asyncdom } from "@/lib/elemental";
 import { Owner, OwnerProfile } from "@/lib/user-values";
 
+const LEGACY_SAFE_REGEX = /jump2?.bdimg.com\/safecheck\//;
+const CURRENT_SAFE_REGEX = /\/mo\/q\/checkurl\?url=/;
+
 export default {
     namespace: "easy-jump",
     title: "直链跳转",
@@ -8,10 +11,10 @@ export default {
     version: "1.0.2",
     brief: "链接跳转避免二次确认",
     description: `自动跳转至分享链接的原始地址，不再进行中转（不处理被严重警告的链接）`,
-    scope: /jump2?.bdimg.com\/safecheck\//,
+    scope: [LEGACY_SAFE_REGEX, CURRENT_SAFE_REGEX],
     runAt: "immediately",
     compatibility: "all",
     async init() {
-        location.href = (await asyncdom<"a">(".link")).innerText;
+        location.href = ((await asyncdom(".link")) as HTMLElement).innerText;
     },
 } satisfies UserModule;
